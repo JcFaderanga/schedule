090c1d4234240e50bfabb4e39219be8ff9af73f2
sol/ <119491303+JcFaderanga@users.noreply.github.com>
2023-02-04 18:44:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+/*modern math*/
+document.getElementById('copylink').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/zbs-oicq-baq");
+});
+/*Purposive com*/
+document.getElementById('copylink2').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/wfp-ckwo-jcb");
+});
+/*Web tech*/
+document.getElementById('copylink3').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/yke-quzm-azm");
+});
+/*Object Oriented Programming*/
+document.getElementById('copylink4').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/grx-cdwg-yrx");
+});
+/*Human Computer Interaction*/
+document.getElementById('copylink5').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/jxv-wkjw-ryk");
+});
+/*System Analysis and Design*/
+document.getElementById('copylink6').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/dnp-ujas-nhy");
+});
+/*Rizal*/
+document.getElementById('copylink7').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/kiz-rppu-qbr");
+});
+/*Entrepreneurial Management*/
+document.getElementById('copylink8').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/wen-bukn-usx");
+});
+/*PE 4*/
+document.getElementById('copylink9').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/nzf-tguf-omi");
+});
+/*Business Software and Tools*/
+document.getElementById('copylink10').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/vtm-jmck-unv");
+});
+/*App dev*/
+document.getElementById('copylink11').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("https://meet.google.com/oaw-tdtk-qkf");
+});
+/*Profdev 4*/
+document.getElementById('copylink12').addEventListener('click', function(){
+	this.innerHTML = "Copied!";
+	navigator.clipboard.writeText("");
+});
